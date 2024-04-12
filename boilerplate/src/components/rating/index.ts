fd0712822ff8b46cbca7c1f78670719784dd6cbd
Rebecca Jeba <rebeccajeba@gmail.com>
2024-04-12 20:38:27 +0200

    stars added to the component

--- boilerplate/src/components/rating/index.ts
+++ boilerplate/src/components/rating/index.ts
@@ -1,13 +1,26 @@
-import { html, LitElement } from 'lit';
+import { html, LitElement, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import star from './star.svg';
 
 @customElement(`case-rating`)
 export class CaseRating extends LitElement {
   @property({ type: String })
   ratingComponentText: string = '';
 
-  render() {
-    return html`<div>${this.ratingComponentText}</div>`;
+  @property({ type: Number })
+  numberOfStars = 5;
+
+  static styles = css`
+  `;
+
+  protected render() {
+    return html`
+    <div> 
+    ${Array.from({ length: this.numberOfStars }, () => {
+      return html`<img class="" src="${star}" alt="star"></img>`
+    })}
+    </div>
+      <div> ${this.ratingComponentText} </div>`
   }
 }
 
